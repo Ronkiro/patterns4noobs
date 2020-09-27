b175deb01c90fa0a627ce9b4e5d7fd21373da458
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-27 15:34:43 -0300

    🚧 adiciona classe CarFactory

---
+++ docs/criacional/factory-method/main/factories/car-factory-model.ts
@@ -0,0 +1,27 @@
+import {ICarFactory} from "../../data/protocols/i-car-factory";
+import {Wheels} from "../../domain/models/wheels-model";
+import {Car} from "../../domain/models/car-model";
+import {Engine} from "../../domain/models/engine-model";
+
+export class CarFactory implements ICarFactory {
+  buildCar(): Car {
+    let car: Car = this.createCar();
+    const wheels = this.addWheels();
+    const engine = this.addEngine();
+    car = { wheels, engine };
+
+    return car;
+  }
+
+  createCar(): Car {
+    return new Car();
+  }
+
+  addEngine(): Engine {
+    return new Engine();
+  }
+
+  addWheels(): Wheels {
+    return new Wheels();
+  }
+}
